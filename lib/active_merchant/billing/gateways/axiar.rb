9ad378f9d4705bb03b3b746fe72a99da4ee3486d
Rob Aldred <raldred@gmail.com>
2009-11-18 11:38:29 +0000

    * fixed bug with xml generation where cart block was in the wrong place * started work on 3d integration based on: http://tinyurl.com/ylmdffl * added register call, to register a transaction for use later

--- lib/active_merchant/billing/gateways/axiar.rb
+++ lib/active_merchant/billing/gateways/axiar.rb
@@ -3,95 +3,146 @@
 module ActiveMerchant
   module Billing
     class AxiarGateway < Gateway
-      API_URL = 'https://api.axiarpayments.co.uk:8081/axiar'
-      
+      API_URL = 'https://127.0.0.1:8081/axiar'
+      # API_URL = 'https://api.axiarpayments.co.uk:8081/axiar'
+      TDS_URL = 'https://api.axiarpayments.co.uk:8081/3dreturn'
+
       self.supported_cardtypes = [:visa, :master, :american_express, :discover, :jcb, :switch, :solo, :maestro, :diners_club]
       self.supported_countries = ['GB']
       self.default_currency = 'GBP'
-      
+      self.supports_3d_secure = true
+
       self.homepage_url = 'http://www.axiarpayments.co.uk/'
       self.display_name = 'Axiar'
-      
+
       self.money_format = :cents
-      
+      self.ssl_strict = false
+
       APPROVED = 'SUCCESS'
-      
+
+      REGISTER_TYPE = 'REGISTER'
       AUTH_TYPE = 'AUTH'
       CANCEL_TYPE = 'REVERSAL'
       FULFILL_TYPE = 'PREAUTH_SETTLE'
       PRE_TYPE = 'PREAUTH'
       REFUND_TYPE = 'REFUND'
-      
+
+      AVS_CVV_CODE = {
+        "U" => 'X',
+        "Y" => 'Y',
+        "N" => 'N'
+      }
+
       def initialize(options = {})
         requires!(options, :login, :password)
         @options = options
         super
-      end  
+      end
       
+      def register(money, options = {})
+        requires!(options, :order_id)
+        commit(build_registration_request(REGISTER_TYPE, money, options))
+      end
+
       def authorize(money, credit_card, options = {})
         requires!(options, :order_id)
         commit(build_authorisation_or_purchase_request(PRE_TYPE, money, credit_card, options))
       end
-      
+
       def purchase(money, credit_card, options = {})
         requires!(options, :order_id)
         commit(build_authorisation_or_purchase_request(AUTH_TYPE, money, credit_card, options))
-      end                       
-    
+      end
+
       def capture(money, reference, options = {})
-        requires!(options, :order_id)        
+        requires!(options, :order_id)
         commit(build_void_or_capture_request(FULFILL_TYPE, money, reference, options))
       end
-      
+
       def void(money, reference, options = {})
-        requires!(options, :order_id)        
+        requires!(options, :order_id)
         commit(build_void_or_capture_request(CANCEL_TYPE, money, reference, options))
       end
-      
+
       def credit(money, credit_card, options = {})
         requires!(options, :order_id)
         commit(build_refund_request(money, credit_card, options))
       end
-      
+
+      # Completes a 3D Secure transaction
+      def three_d_complete(pa_res, md)
+        commit(build_3d_secure_complete_request, 'PaRes' => pa_res, 'MD' => md)
+      end
+
       # Is the gateway running in test mode?
       def test?
         @options[:test] || super
       end
-    
+
+      def three_d_secure_enabled?
+        @options[:enable_3d_secure]
+      end
+
       private
       
+        def build_registration_request(type, money, options)
+          xml = Builder::XmlMarkup.new :indent => 2
+          xml.instruct!
+          xml.tag! :request do
+            add_authentication(xml)
+
+            xml.tag! :options do
+              xml.tag! :type, type
+            end
+            
+            xml.tag! :payment do
+              add_customer_information(xml,options)
+              add_total(xml,money)
+            end
+            
+            xml.tag! :cart do
+              xml.tag! :items, nil
+              xml.tag! :cart_id, options[:order_id]
+            end
+          end
+          xml.target!
+        end
+
         def build_authorisation_or_purchase_request(type, money, credit_card, options)
           xml = Builder::XmlMarkup.new :indent => 2
           xml.instruct!
           xml.tag! :request do
             add_authentication(xml)
-            
+
             xml.tag! :options do
               xml.tag! :type, type
+              xml.tag! :parent_transaction, options[:parent_transaction]
+              unless options[:skip_3d_secure] == true
+                xml.tag! :td_secure, three_d_secure_enabled? ? 1 : ''
+                xml.tag! :td_description, ''
+              end
             end
 
             xml.tag! :payment do
               add_customer_information(xml,options)
               add_credit_card(xml, credit_card, options)
+              add_total(xml,money)
+            end
 
-              xml.tag! :total, amount(money)
-              xml.tag! :currency, 826
-              
-              xml.tag! :cart do
-                xml.tag! :items, nil
-                xml.tag! :cart_id, options[:order_id]
-              end              
-            end            
+            xml.tag! :cart do
+              xml.tag! :items, nil
+              xml.tag! :cart_id, options[:order_id]
+            end
           end
           xml.target!
         end
-        
+
         def build_void_or_capture_request(type, money, authorization, options)
           xml = Builder::XmlMarkup.new :indent => 2
           xml.instruct!
           xml.tag! :request do
             add_authentication(xml)
-            
+
             xml.tag! :options do
               xml.tag! :type, type
               xml.tag! :parent_transaction, authorization
@@ -99,25 +150,23 @@ module ActiveMerchant
 
             xml.tag! :payment do
               add_customer_information(xml,options)
+              add_total(xml,money)
+            end
 
-              xml.tag! :total, amount(money)
-              xml.tag! :currency, 826
-              
-              xml.tag! :cart do
-                xml.tag! :items, nil
-                xml.tag! :cart_id, options[:order_id]
-              end              
-            end            
+            xml.tag! :cart do
+              xml.tag! :items, nil
+              xml.tag! :cart_id, options[:order_id]
+            end
           end
           xml.target!
         end
-        
+
         def build_refund_request(money, credit_card, options)
           xml = Builder::XmlMarkup.new :indent => 2
           xml.instruct!
           xml.tag! :request do
             add_authentication(xml)
-            
+
             xml.tag! :options do
               xml.tag! :type, REFUND_TYPE
             end
@@ -125,35 +174,37 @@ module ActiveMerchant
             xml.tag! :payment do
               add_customer_information(xml, options)
               add_credit_card(xml, credit_card, options)
+              add_total(xml,money)
+            end
 
-              xml.tag! :total, amount(money)
-              xml.tag! :currency, 826
-              
-              xml.tag! :cart do
-                xml.tag! :items, nil
-                xml.tag! :cart_id, options[:order_id]
-              end              
-            end            
+            xml.tag! :cart do
+              xml.tag! :items, nil
+              xml.tag! :cart_id, options[:order_id]
+            end
           end
           xml.target!
-        end        
-        
+        end
+
+        def build_3d_secure_complete_request
+
+        end
+
         def add_authentication(xml)
           xml.tag! :authorisation do
             xml.tag! :username, @options[:login]
             xml.tag! :password, @options[:password]
           end
         end
-        
+
         def add_credit_card(xml, credit_card, options)
 
           xml.tag! :card do
             unless options[:card_token].blank?
-              
+
               xml.tag! :tokenid, options[:card_token]
-              
+
             else
-            
+
               xml.tag! :number, credit_card.number
               xml.tag! :expiry do
                 xml.tag! :month, sprintf('%02d',credit_card.month)
@@ -195,32 +246,42 @@ module ActiveMerchant
             xml.tag! :postcode, address[:zip] unless address[:zip].blank?
           end
         end
-        
+
+        def add_total(xml,money)
+          xml.tag! :total, amount(money)
+          xml.tag! :currency, 826
+        end
+
         def commit(request)
-          
+
           response = parse(ssl_post(API_URL, request))
 
           if response[:avs_result]
-            avs_result = { 
-               :street_match => response[:avs_result][1,1],
-               :postal_match => response[:avs_result][2,1]
+            avs_result = {
+               :street_match => AVS_CVV_CODE[response[:avs_result][1,1]],
+               :postal_match => AVS_CVV_CODE[response[:avs_result][2,1]]
             }
-            cvv_result = response[:avs_result][0,1]
+            cvv_result = AVS_CVV_CODE[response[:avs_result][0,1]]
           end
 
           Response.new(response[:result] == APPROVED, response[:error_message] || response[:auth_message], response,
             :test => test?,
             :authorization => "#{response[:trx_id]};#{response[:auth_code]}",
             :avs_result => avs_result,
-            :cvv_result => cvv_result
+            :cvv_result => cvv_result,
+            :three_d_secure => (response[:pareq] and response[:md] and response[:redirect_url]) ? true : false,
+            :pa_req => response[:pareq],
+            :md => response[:md],
+            :acs_url => response[:redirect_url]
           )
         end
-      
+
         def parse(body)
-          
+
           response = {}
           xml = REXML::Document.new(body)
           root = REXML::XPath.first(xml, "//response")
+          root = REXML::XPath.first(xml, "//tds_response") if root.nil?
           root = REXML::XPath.first(xml, "//error") if root.nil?
 
           root.elements.to_a.each do |node|
